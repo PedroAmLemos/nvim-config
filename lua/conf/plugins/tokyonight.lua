6124530202c89088ab8e55874d12bee0f5432a34
pedroamlemos <peedroamlemos@gmail.com>
2022-12-28 00:42:10 -0300

    colocando borda no telescope

--- lua/conf/plugins/tokyonight.lua
+++ lua/conf/plugins/tokyonight.lua
@@ -24,42 +24,6 @@ function M.config()
     transparent = false,
     styles = {},
     on_colors = function(c) end,
-    on_highlights = function(hl, c)
-      -- make the current line cursor orange
-      hl.CursorLineNr = { fg = c.orange, bold = true }
-
-      if true then
-        -- borderless telescope
-        local prompt = "#2d3149"
-        hl.TelescopeNormal = {
-          bg = c.bg_dark,
-          fg = c.fg_dark,
-        }
-        hl.TelescopeBorder = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-        hl.TelescopePromptNormal = {
-          bg = prompt,
-        }
-        hl.TelescopePromptBorder = {
-          bg = prompt,
-          fg = prompt,
-        }
-        hl.TelescopePromptTitle = {
-          bg = c.fg_gutter,
-          fg = c.orange,
-        }
-        hl.TelescopePreviewTitle = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-        hl.TelescopeResultsTitle = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-      end
-    end,
   })
 
   tokyonight.load()
